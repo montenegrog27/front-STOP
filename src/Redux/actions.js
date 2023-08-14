d9197400b21a0c7062e8592bae8c0367de4aa914
german2717@gmail.com <german2717@gmail.com>
2023-08-14 09:13:30 -0300

    delete

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -78,9 +78,10 @@ const postearOferta = (products) => {
   };
 };
 const eliminarOferta = (id) => {
+  console.log("EL ID", id);
   return async (dispatch) => {
     try {
-      await axios.delete(`${URL}/${id}`);
+      await axios.delete(`${URL}/ofertas/${id}`);
       dispatch({
         type: ELIMINAR_OFERTA,
         payload: id, // Envía el ID de la oferta eliminada al reducer
