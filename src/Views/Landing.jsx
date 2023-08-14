428ba2d67196bd8161e30bc629030cbb6354d700
German <german2717@gmail.com>
2023-08-14 00:04:18 -0300

    listo

--- src/Views/Landing.jsx
+++ src/Views/Landing.jsx
@@ -4,7 +4,11 @@ function Landing() {
   return (
     <>
       <div className="flex justify-center items-center">
-        <img className="h-screen object-cover w-screen" src={landing} alt="" />
+        <img
+          className="h-screen lg:object-cover w-auto lg:w-screen"
+          src={landing}
+          alt=""
+        />
         <button className="absolute mt-[350px] px-6 py-3 rounded-[30px] font-bold text-[25px] text-gray-100 letra hover:scale-110 transition border-2  rojol2 ">
           <Link to={"/home"}>Ingresar</Link>
         </button>
